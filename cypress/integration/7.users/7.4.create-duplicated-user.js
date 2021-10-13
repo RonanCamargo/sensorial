b185419e0991622cde28b555f90b4c4923ce4322
RonanCamargoDaruma <91966367+RonanCamargoDaruma@users.noreply.github.com>
2021-10-13 19:27:40 -0300

    Add test for duplicated users

---
+++ cypress/integration/7.users/7.4.create-duplicated-user.js
@@ -0,0 +1,15 @@
+import validUser from "../../fixtures/data/admin-user.json"
+import newOperativeUser from "../../fixtures/data/new-operative-user.json"
+import { createUser, login, logout } from "../../fixtures/helpers/users"
+import { loginPage } from "../../fixtures/shared"
+
+describe('7. Crear y administrar usuarios', () => {
+    it('7.4 - Crear usuario duplicado', () => {
+        loginPage()
+
+        login(validUser)
+        createUser(newOperativeUser)
+        createUser(newOperativeUser, true)
+        
+    })
+})
