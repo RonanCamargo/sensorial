30f90eb51187586d40b38637a54154909e8f47c8
RonanCamargoDaruma <91966367+RonanCamargoDaruma@users.noreply.github.com>
2021-11-19 19:47:53 -0300

    Go to login page

--- cypress/integration/7.users/7.1.create-admin.js
+++ cypress/integration/7.users/7.1.create-admin.js
@@ -1,11 +1,12 @@
 import newAdminUser from "../../fixtures/data/new-admin-user.json"
 import validUser from "../../fixtures/data/admin-user.json"
 import { createUser, deleteUser, login, logout } from "../../fixtures/helpers/users"
+import { loginPage } from "../../fixtures/shared"
 
 describe('7. Crear y administrar usuarios', () => {
  
     it('7.1 - Crear usuario administrador', () => {
-        cy.visit('localhost')
+        loginPage()
 
         login(validUser)
         createUser(newAdminUser)
